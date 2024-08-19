67daefb284488c8b88c13b33e77027931463fb73
satyam <satyamkale271@gmail.com>
2024-08-19 18:06:55 +0530

    react router used

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+
 function App() {
-  return <div>world wise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
