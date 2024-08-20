2045fe61193d42f9053baeca0db8a2c7427cd908
satyam <satyamkale271@gmail.com>
2024-08-20 18:24:06 +0530

    css modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 function Homepage() {
   return (
     <div>
       <PageNav />
-      <Link to="/pricing">Pricing</Link>
+      <AppNav />
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
