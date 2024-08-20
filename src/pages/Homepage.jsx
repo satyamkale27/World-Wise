0d9a57b8128e94870292107024e49d3829897bc9
satyam <satyamkale271@gmail.com>
2024-08-20 17:16:30 +0530

    Link used

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
 function Homepage() {
-  return <div>worldwise</div>;
+  return (
+    <div>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default Homepage;
