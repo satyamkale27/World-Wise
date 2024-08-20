c087496c939b5a6d7f971530ce6aac598d8fed24
satyam <satyamkale271@gmail.com>
2024-08-20 17:29:11 +0530

    navbar

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 function Homepage() {
   return (
     <div>
+      <PageNav />
       <Link to="/pricing">Pricing</Link>
     </div>
   );
