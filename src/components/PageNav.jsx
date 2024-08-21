dbed0a491f8d924b13be3de0a4d2374c5d079cf4
satyam <satyamkale271@gmail.com>
2024-08-21 14:37:07 +0530

    link to logo

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,17 +1,19 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo";
 function PageNav() {
   return (
     <nav className={styles.nav}>
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
+        <Logo />
         <li>
           <NavLink to="/pricing">pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
