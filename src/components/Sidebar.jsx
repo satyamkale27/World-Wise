e646a0c177f49980dfcb62b32f14b06574d0d29d
satyam <satyamkale271@gmail.com>
2024-08-21 15:42:36 +0530

    used nested routes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,13 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
+      <Outlet />
       <p>List of cities</p>
       <footer className={styles.footer}>
         <p className={styles.copyright}>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,4 +1,3 @@
-import AppNav from "../components/AppNav";
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map";
