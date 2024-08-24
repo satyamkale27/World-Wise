81c1d399919bd37e27b53d237d85534d23ed0689
satyam <satyamkale271@gmail.com>
2024-08-24 19:28:31 +0530

    move back in navigate

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate to="cities" />} />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
