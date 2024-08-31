80074e421673454e2c2c06f93e79e612986b6927
satyam <satyamkale271@gmail.com>
2024-08-31 14:53:38 +0530

    api to fetch city by lat and lnd and set the setcity

--- src/hooks/useUrlPosition.js
+++ src/hooks/useUrlPosition.js
@@ -1,6 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 
 export function useUrlPosition() {
+  // custom hook //
   const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
